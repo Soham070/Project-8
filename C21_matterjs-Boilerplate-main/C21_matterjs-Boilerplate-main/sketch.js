4df9e8407465e4893e5f170064ebcf3ac7105c8f
Soham070 <85925591+Soham070@users.noreply.github.com>
2021-08-10 13:12:10 +0530

    Add files via upload

---
+++ C21_matterjs-Boilerplate-main/C21_matterjs-Boilerplate-main/sketch.js
@@ -0,0 +1,15 @@
+constructor (body1,body2,pointA,pointB)
+
+rope1=new rope(bob1,roof,-80,0);
+ 
+display()
+{
+
+  var pointA=this.rope.bodyA.position;
+  var pointB=this.rope.bodyB.position;
+
+  strokeWeight(2);
+  //write line () code below to show line between roof and bob
+
+
+}
